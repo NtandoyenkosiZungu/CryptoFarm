a9ddc5187220b6acf3db1cb0926e2119af35051b
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-09-13 12:53:56 +0200

    Adding the cryptofarm component

---
+++ src/components/CryptoFarm.jsx
@@ -0,0 +1,44 @@
+
+
+
+export const CryptoFarm = () => {
+    return (
+        <div className="w-9/10 h-15 bg-amber-50 border-2 border-black p-5 relative rounded-xl  flex gap-6">
+            <div className="absolute top-3">
+                <img 
+                    src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400" 
+                    alt="" 
+
+                    className="size-9 "
+                />
+            </div>
+            <div className="flex gap-4 absolute left-1/5">
+                <span id="price">
+                    {"$116,121"}
+                </span>
+
+                <span id="1h">
+                    {"0.8%"}
+                </span>
+
+                <span id="24h">
+                    {"1.3%"}
+                </span>
+
+                <span id="7d">
+                    {"5.7%"}
+                </span>
+
+                <span>
+
+                </span>
+            </div>
+
+            <button
+                className=" absolute bg-green-500 hover:bg-green-600 text-white font-semibold  w-25 p-2 right-5 bottom-2 rounded-lg shadow-black hover:translate-y-[-3px] transition-all active:translate-y-0"
+            >
+                Plant
+            </button>
+        </div>
+    )
+}
