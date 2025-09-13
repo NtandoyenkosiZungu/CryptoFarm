661b8f7058a5098f3f985f124727b9762a48b775
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-09-13 16:00:44 +0200

    Updated tree display on the garden componennt

--- src/components/CryptoTree.jsx
+++ src/components/CryptoTree.jsx
@@ -1,11 +1,12 @@
+import tree1 from "../assets/Tree 15/TREE 15_YELLOw.png"
 
-import tree1 from '../assets/Tree 1/TREE 1_GREEN.png'
 
 const CryptoTree = () => {
     
     return (
-        <div className="size-35 bg-green-400">
-            <img src={tree1} alt="" />
+        <div className="size-30 flex items-center justify-center rounded-lg   p-2">
+
+            <img src={tree1} alt=""  className="hover:transform hover:scale-110 transition-transform duration-300 cursor-pointer active:scale-100"/>
         </div>
     )
 }
