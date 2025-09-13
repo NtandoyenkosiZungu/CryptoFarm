9860dbd04d993235eef610ab2c6899dc71de696c
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-09-13 15:40:05 +0200

    Placing the tree assets in the correct directory

--- src/components/CryptoTree.jsx
+++ src/components/CryptoTree.jsx
@@ -1,11 +1,11 @@
 
-
+import tree1 from '../assets/Tree 1/TREE 1_GREEN.png'
 
 const CryptoTree = () => {
     
     return (
         <div className="size-35 bg-green-400">
-            <img src="" alt="" />
+            <img src={tree1} alt="" />
         </div>
     )
 }
